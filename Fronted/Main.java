9ca283385fed4f4b5acacd85f1f9ed1ff8524407
Isaac Hu <isaac.hu002@gmail.com>
2024-05-03 12:51:46 -0400

    minor change

--- Fronted/Main.java
+++ Fronted/Main.java
@@ -3,5 +3,7 @@ package Fronted;
 public class Main {
         public static void main(String[] args) {
                 new LoginPage();
+
+                System.out.println("Hello!");
         }
 }
